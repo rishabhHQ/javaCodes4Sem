5245dd56a9ab6aace329e6265f8e2cebfad08635
rishabh-24bcs10110 <24bcs10110@cuchd.in>
2026-01-19 19:31:59 +0530

    just added code i will check and add comments later on

---
+++ arrey.java
@@ -0,0 +1,11 @@
+class jj {
+    public static void main(String... abc) {
+        int[] a = {1,2,3,4,5};
+        int i = 0; {
+            for(;i<a.length;) {
+                System.out.println(a[i]);
+                i++;
+            }
+        }
+    }
+}
