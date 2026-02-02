c850063b90fa7dcd923811982a99bcc4d0eafb0b
Rishabh Paudel <24BCS10110@cuchd.in>
2026-02-02 19:48:17 +0530

    added methodFunctionDifference1.java

---
+++ methodFunctionDifference1.java
@@ -0,0 +1,18 @@
+class xyz {
+    int x = 5;
+    xyz() {
+        System.out.println("Default Constructor");
+    } 
+    xyz(int a) {
+        System.out.println("Default Constructor:- "+a);
+    }
+}
+class methodFunctionDifference1 {
+    public static void main(String[] args) {
+        xyz x = new xyz();
+        xyz x1 = new xyz(3);
+    }
+}
+
+// as far as I remember in this it was like when we have to call a method(function) we have to create different
+// different objects everytime which in return takes up more time and space complixity so therefore we use "this".
