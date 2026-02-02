1b3fd7e0eb4e8af35f12e79adb1c8b791e898ac3
Rishabh Paudel <24BCS10110@cuchd.in>
2026-02-02 19:38:40 +0530

    added methofFunctionDiffrence.java

---
+++ methodFunctionDifference.java
@@ -0,0 +1,10 @@
+class xyz {
+    xyz() {
+        System.out.println("Default Constructoe");
+    }
+}
+class methodFunctionDifference {
+    public static void main(String[] args) {
+        xyz x = new xyz();
+    }
+}
