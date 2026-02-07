d73c64009555b46b3d041a1ae2b8a1c6181c0cbb
Rishabh Paudel <24BCS10110@cuchd.in>
2026-02-07 19:39:53 +0530

    added anonymus.java

---
+++ 06-02-2026/anonymusClass.java
@@ -0,0 +1,17 @@
+class innerA {
+    void display() {
+        class innerB {
+            void show() {
+                System.out.println("inner classB");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        innerA inA = new innerA();
+        inA.display();
+        // rest code I dont have if someone can complete it please do
+    }
+}
+
+// in this the inner class is inside a method(function) ..rest i can only say after seeing complete code
+// which I dont have at the moment 
