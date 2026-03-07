b78e09f59b27b14339f90c21a5cebe99616a3d79
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-07 23:46:31 +0530

    completed outputstream  code but didnt checked it its work or not

--- 03-03-2026/outputstream.java
+++ 03-03-2026/outputstream.java
@@ -1,5 +1,18 @@
+// File Created on : 3/3/2026
+// last edited on: 7/03/2026
+
 import java.io.*;
+import java.util.*;
 
 class ipstream1 {
-    // complete later
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        FileOutputStream fos = new FileOutputStream("ops1");
+        System.out.print("Enter Data to save in file: ");
+        String s = sc.nextLine();
+        fos.write(s.getBytes());            // hmm sometin new
+
+        fos.close();
+        sc.close();
+    }
 }
