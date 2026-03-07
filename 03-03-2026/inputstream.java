8cf450a3287e54e913bef6c2885f4e33311aa267
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-07 23:40:30 +0530

    completed ipstream code but somwhat its wrong

--- 03-03-2026/inputstream.java
+++ 03-03-2026/inputstream.java
@@ -1,5 +1,18 @@
-import java.io.*;
+// File Created on : 3/3/2026
+// last edited on: 7/03/2026
 
+import java.io.*;
 class ipstream1 {
-    // complete later
+    public static void main(String[] args) 
+        throws IOException {
+
+            FileInputStream fis = new FileInputStream("ips1.txt");
+            int i;
+            // sometin is wrin in below code
+            while(i = fis.read()) {
+                System.out.print((char)i);
+            }
+
+         }
+    
 }
